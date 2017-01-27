07a9ffd5e9afd86ac451f76e011a25a18f7e793f
James Go <hun.go@ca.com>
2017-01-27 14:41:12 -0800

    MASUI - stopping the activity indicator within main thread

--- MASUI/Classes/_private_/ui/login/MASLoginViewController.m
+++ MASUI/Classes/_private_/ui/login/MASLoginViewController.m
@@ -434,21 +434,6 @@
     [self loginWithAuthorizationCode:code completion:^(BOOL completed, NSError *error) {
         
         //
-        // Stop progress animation
-        //
-        [blockSelf.activityIndicator stopAnimating];
-        
-        //
-        // Handle the error
-        //
-        if(error)
-        {
-            [UIAlertController popupErrorAlert:error inViewController:blockSelf];
-            
-            return;
-        }
-        
-        //
         // Ensure this code runs in the main UI thread
         //
         dispatch_async(dispatch_get_main_queue(), ^
@@ -457,6 +442,16 @@
                            // Stop progress animation
                            //
                            [blockSelf.activityIndicator stopAnimating];
+                           
+                           //
+                           // Handle the error
+                           //
+                           if(error)
+                           {
+                               [UIAlertController popupErrorAlert:error inViewController:blockSelf];
+                               
+                               return;
+                           }
                            
                            //
                            // Dsmiss the view controller
